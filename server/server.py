79b3199f17852bcaa9813675edeaade686c44090
ysnacrk <ysnacrk@gmail.com>
2020-07-08 14:51:06 +0300

    Connection error fixed

--- server/server.py
+++ server/server.py
@@ -38,7 +38,7 @@ def run():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_pb2_grpc.add_UserServiceServicer_to_server(
         UserServicer() , server)
-    server.add_insecure_port('0.0.0.0:50051') 
+    server.add_insecure_port('[::]:50051') 
     server.start() 
     server.wait_for_termination()
 
